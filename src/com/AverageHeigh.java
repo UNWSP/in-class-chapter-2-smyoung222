03333c1f843f2e7bf52057ce1dd9a2407f33254a
smyoung222 <youngstephen599@gmail.com>
2022-09-01 18:36:34 -0500

    average height
    inclass project

---
+++ src/com/AverageHeigh.java
@@ -0,0 +1,26 @@
+package com;
+
+public class AverageHeigh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int student_1 = 70;
+		int student_2 = 72;
+		int student_3 = 70;
+		int student_4 = 72;
+		int student_5 = 71;
+		float avg = 0;
+		
+		int sum = student_1 + student_2 + student_3 + student_4 + student_5;
+		
+		avg = sum/5;
+				
+		System.out.println("average height = " + avg);
+		
+		
+				
+
+	}
+
+}
