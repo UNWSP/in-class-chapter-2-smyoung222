a44b2ef9d5aca2c33f64909ac40245d253d8d143
smyoung222 <youngstephen599@gmail.com>
2022-09-01 21:02:16 -0500

    tickets completed
    slow and steady

---
+++ src/com/Scopes.java
@@ -0,0 +1,12 @@
+package com;
+
+public class Scopes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int TRY_TO_CHANGE = 30;
+		
+
+	}
+
+}
--- src/com/calcTwinsTix.java
+++ src/com/calcTwinsTix.java
@@ -6,12 +6,27 @@ public class calcTwinsTix {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int number_tix_int;
 		final int TICKET = 75;
 		final int FEE = 20;
-	    final double TAX = 1.07;
-	    float TOTAL = (float) (TICKET + FEE * TAX);
+	    final double TAX = .07;
+	   // float TOTAL = (float) (TICKET + FEE * TAX);
+	    
+	    String ticket_number = JOptionPane.showInputDialog("How many tickets?");
+	    number_tix_int = Integer.parseInt(ticket_number);
+	    int ticket_prices = (TICKET * number_tix_int);
+	    int subtotal = (ticket_prices + FEE);
+	    double taxes = (subtotal * TAX);
+	    double total = (subtotal + taxes);
+	    System.out.println("Total cost of tickets..." + total);
+	    
+
 	    
 	    
+	    
+	    
+	    
+	   //System.out.println(TOTAL);
 	    
 	    
 
