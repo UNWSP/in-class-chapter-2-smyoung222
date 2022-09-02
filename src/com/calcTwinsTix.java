5c8568017846018360d86daedcfe5a9c26f7223a
smyoung222 <youngstephen599@gmail.com>
2022-09-01 19:18:14 -0500

    in class
    in class assignment, didnt have time to catch up, will edit when over the weekend

---
+++ src/com/calcTwinsTix.java
@@ -0,0 +1,24 @@
+package com;
+
+import javax.swing.JOptionPane;
+
+public class calcTwinsTix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int TICKET = 75;
+		final int FEE = 20;
+	    final double TAX = 1.07;
+	    float TOTAL = (float) (TICKET + FEE * TAX);
+	    
+	    
+	    
+	    
+
+	    
+	
+	    
+
+	}
+
+}
